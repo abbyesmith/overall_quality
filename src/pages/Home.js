7c37c948c7a4ac7100e984f2ef6e84aa50ec736b
Abby <abby.e.smith.85@gmail.com>
2023-08-28 13:40:40 -0600

    removed comment about url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
         <>
             <Hero />
             <p className = "font-bold bg-red-500 text-white">Hey guys! Here is my first draft of your site. It is still a work in progress. For example, you'll find that some buttons don't work yet, I want to get a gallery of work going. The email service is not yet set up. Everything can change, but I wanted to put something in front of you so you can see a possible outline of the site.</p>
-            <p className = "font-bold bg-red-500 text-white">The url will not include the github aspect once we get it ironed out. I'm using a site that makes hosting live websites easy to edit until we reach a nice finished product.</p>
+            {/* <p className = "font-bold bg-red-500 text-white">The url will not include the github aspect once we get it ironed out. I'm using a site that makes hosting live websites easy to edit until we reach a nice finished product.</p> */}
 
             <Intro />
             <Services />
