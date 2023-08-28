41241ff6fe38e0333f963b4afb8794586a003277
Abby <abby.e.smith.85@gmail.com>
2023-08-28 13:51:44 -0600

    commented out porftolio on home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Cta from '../components/Cta';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Intro from '../components/Intro';
-import Portfolio from '../components/Portfolio';
+// import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 
 
